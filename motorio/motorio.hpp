33dc14a17951feb28dc05cb51de0595f690a167a
rotarymars <s.goto2050@gmail.com>
2025-04-21 20:09:42 +0900

    motorio/motorio.cpp,motorio/motorio.hpp: Made the code dirty as a test

--- motorio/motorio.hpp
+++ motorio/motorio.hpp
@@ -3,12 +3,7 @@
 #include <Arduino.h>
 class MOTORIO {
  public:
-  MOTORIO();
-  MOTORIO(const std::int8_t&);
-  MOTORIO& operator=(const MOTORIO&) = default;
-  void run_msec(const int&);
-
- private:
-  std::int8_t PIN;
-};
+  MOTORIO();MOTORIO(const std::int8_t&);MOTORIO& operator=(const MOTORIO&) = default;void run_msec(const int&);
+private:
+ std::int8_t PIN;};
 #endif
