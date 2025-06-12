401fe09b76dd04d18d949cc0561ea8aed095d513
rotarymars <s.goto2050@gmail.com>
2025-06-12 16:35:51 +0900

    initialized serial_io.hpp nad serial_io.cpp

---
+++ serial_io/serial.cpp
@@ -0,0 +1,38 @@
+#include "serial_io.hpp"
+
+SerialIO::SerialIO() {
+  init();
+  isReady = false;
+}
+
+void SerialIO::init() {
+  Serial.begin(9600);
+  while (true) {
+    if (Serial.available()) {
+      String str = Serial.readStringUntil('\n');
+      if (str == "[RASPI] READY?") {
+        Serial.println("[ESP32] READY");
+      }
+      else if (str == "[RASPI] READY CONFIRMED") {
+        isReady = true;
+        break;
+      }
+    }
+  }
+}
+
+void SerialIO::sendMessage(const String& message) {
+  Serial.println(message);
+}
+
+void SerialIO::sendMessage(const char* message) {
+  Serial.println(message);
+}
+
+String SerialIO::receiveMessage() {
+  return Serial.readStringUntil('\n');
+}
+
+bool SerialIO::isMessageAvailable() {
+  return Serial.available() > 0;
+}
---
+++ serial_io/serial_io.hpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+class SerialIO {
+  public:
+    SerialIO();
+    void sendMessage(const String& message);
+    void sendMessage(const char* message);
+    String receiveMessage();
+    bool isMessageAvailable();
+    void init();
+  private:
+    bool isReady = false;
+};
