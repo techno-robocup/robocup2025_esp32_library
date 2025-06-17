178f7a7eb6ecbb168af9611cba9ee09767ed9a34
K10-K10 <keito.tottori@gmail.com>
2025-06-17 17:04:35 +0900

    Update

--- arm_io/arm.cpp
+++ arm_io/arm.cpp
@@ -1,4 +1,21 @@
 #include <arm.hpp>
 
-constexpr int wire = 32;
-constexpr int arm_pulse = 17, arm_feedback = 34;
+constexpr int wire_pin = 32;
+constexpr int arm_pin = 17;
+constexpr int arm_feedback = 34;
+
+MOTORIO::MOTORIO(const std::int8_t& _PIN) : PIN(_PIN) { pinMode(PIN, OUTPUT); }
+
+void MOTORIO::write(int degree) {
+  int pulse = map(degree, 0, 180, 500, 2500);
+  ledcWrite(PIN, pulse);
+}
+
+MOTORIO wire(wire_pin);
+MOTORIO arm(arm_pin);
+
+// Arm関数
+void Arm() {
+  wire.write(90);
+  arm.write(90);
+}
--- arm_io/arm.hpp
+++ arm_io/arm.hpp
@@ -1,1 +1,7 @@
 #include <Arduino.h>
+
+class ARMIO {
+ public:
+  int wire_digit();
+  int arm_digit();
+}
