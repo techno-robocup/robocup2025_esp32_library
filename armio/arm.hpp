f4d59f8b93f16a06fd1c397d8e6b3feecb4f0b6b
K10-K10 <keito.tottori@gmail.com>
2025-06-18 16:21:05 +0900

    Add .hpp

---
+++ armio/arm.hpp
@@ -0,0 +1,16 @@
+#ifndef __ROBOT__ARMIO__HPP
+#define __ROBOT__ARMIO__HPP 1
+#include <Arduino.h>
+
+class ARMIO {
+ public:
+  ARMIO();
+  ARMIO(const std::int8_t&);
+  ARMIO& operator=(const ARMIO&) = default;
+  void wire_pulse(const int& tension);
+  void arm_position(const int position) {}
+
+ private:
+  std::int8_t PIN;
+}
+#endif
