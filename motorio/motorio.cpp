681760c4017717a0ac6dab57662f13a079c99004
rotarymars <s.goto2050@gmail.com>
2025-06-19 16:54:36 +0900

    rewrote motorio.cpp for future multithread

--- motorio/motorio.cpp
+++ motorio/motorio.cpp
@@ -1,11 +1,20 @@
 #include <motorio.hpp>
 
-MOTORIO::MOTORIO(const std::int8_t& _PIN) : PIN(_PIN) { pinMode(PIN, OUTPUT); }
+MOTORIO::MOTORIO(const std::int8_t& _PIN) : PIN(_PIN), prev_msec(micros()), is_running(false){ pinMode(PIN, OUTPUT); }
 
 MOTORIO::MOTORIO() {}
 void MOTORIO::run_msec(const int& msec) {
-  digitalWrite(PIN, HIGH);
-  delayMicroseconds(msec);
-  digitalWrite(PIN, LOW);
+  if(is_running) {
+    if(micros() - prev_msec > msec) {
+      digitalWrite(PIN, LOW);
+      is_running = false;
+      prev_msec = micros();
+    }
+  }
+  else {
+    digitalWrite(PIN, HIGH);
+    is_running = true;
+    prev_msec = micros();
+  }
   return;
 }
