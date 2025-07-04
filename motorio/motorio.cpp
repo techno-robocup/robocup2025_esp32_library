b3937380502c5cbbd6ccde06db7b0bb4b15d916d
rotarymars <s.goto2050@gmail.com>
2025-07-04 15:32:51 +0900

    fixed operator

--- motorio/motorio.cpp
+++ motorio/motorio.cpp
@@ -8,7 +8,7 @@ MOTORIO::MOTORIO(const std::int8_t& _PIN, const int& _interval)
 MOTORIO::MOTORIO() {}
 void MOTORIO::run_msec(const int& msec) {
   if (is_running) {
-    if (micros() - prev_msec > msec) {
+    if (micros() - prev_msec >= msec) {
       digitalWrite(PIN, LOW);
       is_running = false;
       prev_msec = micros();
