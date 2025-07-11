f937250271aab25c2a4ddcb5b4754b7bb949ac8d
rotarymars <s.goto2050@gmail.com>
2025-07-11 14:51:34 +0900

    library for ultrasonic

---
+++ ultrasonic_io/ultrasonic_io.cpp
@@ -0,0 +1,16 @@
+#include "ultrasonic_io.hpp"
+
+UltrasonicIO::UltrasonicIO(int _trig, int _echo) : trig(_trig), echo(_echo) {
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
+
+void UltrasonicIO::read(long * values) {
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  long duration = pulseIn(echo, HIGH);
+  duration*=0.000001*3400;
+  duration/=2.0;
+  *values = duration;
+}
