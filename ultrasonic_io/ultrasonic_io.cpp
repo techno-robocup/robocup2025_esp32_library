6f21c74ce9bc5deb44b3b38d71692e010d61b8dc
rotarymars <s.goto2050@gmail.com>
2025-07-11 20:10:12 +0900

    Update ultrasonic_io.cpp

--- ultrasonic_io/ultrasonic_io.cpp
+++ ultrasonic_io/ultrasonic_io.cpp
@@ -9,6 +9,7 @@ void UltrasonicIO::read(long* values) {
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
+  // duration: centimeter
   long duration = pulseIn(echo, HIGH);
   duration *= 0.000001 * 3400;
   duration /= 2.0;
