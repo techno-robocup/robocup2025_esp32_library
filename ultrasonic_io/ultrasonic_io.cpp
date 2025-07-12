ba1ff38b5e5b8cfa78d3602e8309f051000c21a2
rotarymars <s.goto2050@gmail.com>
2025-07-12 16:56:36 +0900

    fixed int to float(or maybe it's going to be double, or long double)

--- ultrasonic_io/ultrasonic_io.cpp
+++ ultrasonic_io/ultrasonic_io.cpp
@@ -12,6 +12,6 @@ void UltrasonicIO::read(long* values) {
   long duration = pulseIn(echo, HIGH);
   // Optimized: Use integer arithmetic instead of floating-point
   // 0.000001 * 3400 / 2.0 = 0.0017, so multiply by 17 and divide by 10000
-  duration = (duration * 17) / 10000;
+  duration = (duration * 17.0) / 10000.0;
   *values = duration;
 }
